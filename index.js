bb802ea191da2e4f24b8654b421929b2de27d539
PRAJWAL S DEVADIGA <imprjde7@gmail.com>
2024-11-14 15:18:39 +0530

    Added logs to both the GET and POST API

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const port = 3000;
 
+app.use(morgan("dev"));
 app.use(express.json());
 
 // -- GET API with query and path parameters -- //
@@ -9,7 +11,14 @@ app.use(express.json());
 app.get("/api/:pathParam", (req, res) => {
   const pathParam = req.params.pathParam;
   const queryParam = req.query.queryParam;
+
+  // Below code logs the details of the incoming GET request
+  console.log("GET Request received");
+  console.log("Path Parameter:", pathParam);
+  console.log("Query Parameter:", queryParam);
+
   res.json({
+    message: `Received path parameter: ${pathParam} and query parameter: ${queryParam}`,
     pathParam,
     queryParam,
   });
@@ -19,6 +28,10 @@ app.get("/api/:pathParam", (req, res) => {
 
 app.post("/api/data", (req, res) => {
   const data = req.body;
+
+  // Log the incoming POST request details
+  console.log("POST Request received");
+  console.log("Request Body:", JSON.stringify(data, null, 2));
 
   res.json({
     info: "POST request with JSON data",
